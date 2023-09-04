b95404be0814971b46a3cbdac6c3d9e458ea087b
kifle23 <kiflekifle23@gmail.com>
2023-09-04 16:29:36 +0300

    Update user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
- 
-
-  validates :password, presence: true
-
-  def recent_posts(limit = 3)
-    posts.order(created_at: :desc).limit(limit)
-  end
-
-  def admin?
-    role == 'admin'
-  end
 end
