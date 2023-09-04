027ef1388268317b821de858b29700cc838417b8
kifle23 <kiflekifle23@gmail.com>
2023-09-04 16:01:44 +0300

    Generate devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+ 
 
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
-
-  validates :name, presence: true
-  validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :password, presence: true
 
   def recent_posts(limit = 3)
