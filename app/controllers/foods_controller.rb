e3925788b0686c0e4b65be0ff68d72cd93dc3117
kifle23 <kiflekifle23@gmail.com>
2023-09-05 20:17:54 +0300

    Update food controller to use devise user id

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -22,6 +22,7 @@ class FoodsController < ApplicationController
   # POST /foods
   def create
     @food = Food.new(food_params)
+    @food.user = current_user
 
     respond_to do |format|
       if @food.save
