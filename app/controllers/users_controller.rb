1ca4c75d78758ff07b56af2da2581536565182de
Denio Nhanale <dnhanale@gmail.com>
2023-09-05 14:19:53 +0200

    Fix Linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,3 @@
 class UsersController < ApplicationController
-    def index; end
+  def index; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  # validates :name, presence: true  
+  # validates :name, presence: true
 end
