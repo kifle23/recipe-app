34f510981edfe9af9bd7ac86a98566d75f28298e
kifle23 <kiflekifle23@gmail.com>
2023-09-05 20:07:29 +0300

    Update foods controller class

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,12 +1,12 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[ show edit update destroy ]
 
-  # GET /foods or /foods.json
+  # GET /foods
   def index
     @foods = Food.all
   end
 
-  # GET /foods/1 or /foods/1.json
+  # GET /foods/1
   def show
   end
 
@@ -19,7 +19,7 @@ class FoodsController < ApplicationController
   def edit
   end
 
-  # POST /foods or /foods.json
+  # POST /foods
   def create
     @food = Food.new(food_params)
 
@@ -34,36 +34,21 @@ class FoodsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: "Food was successfully updated." }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /foods/1 or /foods/1.json
+  # DELETE /foods/1
   def destroy
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: "Food was successfully destroyed." }
+      format.html { redirect_to foods_url, notice: "Food was successfully deleted." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_food
       @food = Food.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def food_params
       params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
     end
