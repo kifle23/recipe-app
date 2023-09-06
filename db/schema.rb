2f7f765b85ee70c344afa6b2e3ffae1bd153acee
Denio Nhanale <dnhanale@gmail.com>
2023-09-06 12:31:17 +0200

    Update Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_074038) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_102212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,12 +27,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_074038) do
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
-    t.bigint "user_id", null: false
     t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
-    t.index ["user_id"], name: "index_recipe_foods_on_user_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -66,6 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_074038) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
-  add_foreign_key "recipe_foods", "users"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
