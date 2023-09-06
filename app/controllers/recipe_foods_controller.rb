3ed37223cc93497f75d52b59a7670aec5f64bf27
Denio Nhanale <dnhanale@gmail.com>
2023-09-06 12:49:05 +0200

    Update Recipe Foods Controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,57 +1,60 @@
 class RecipeFoodsController < ApplicationController
-  before_action :find_recipe, only: %i[new create edit update]
-
   def new
+    @recipe = Recipe.find_by(id: params[:recipe_id])
     @recipe_food = RecipeFood.new
   end
 
   def create
-    @food_ids = @recipe.recipe_foods.pluck(:food_id)
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @food_ids = RecipeFood.where(recipe_id: @recipe.id).map(&:food_id)
 
     if @food_ids.include?(recipe_food_params[:food_id].to_i)
-      redirect_to recipe_path(@recipe), alert: 'Recipe already has this ingredient!'
-    else
-      @recipe_food = @recipe.recipe_foods.build(recipe_food_params)
-
-      if @recipe_food.save
-        redirect_to recipe_path(@recipe), notice: 'Ingredient added successfully'
-      else
-        flash[:alert] = 'Failed to add ingredient'
-        render :new
-      end
+      return redirect_to recipe_path(@recipe.id),
+                         alert: 'Recipe already has this ingredient!'
     end
+
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe_food.recipe_id = @recipe.id
+
+    if @recipe_food.save
+      flash[:notice] = 'Ingredients added successfully'
+    else
+      flash[:alert] = 'Failed to add Ingredients!'
+    end
+    redirect_to recipe_path(@recipe)
   end
 
   def edit
-    @recipe_food = RecipeFood.find_by(id: params[:id])
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @recipe_food = RecipeFood.includes(:recipe).find_by(id: params[:id])
   end
 
   def update
-    @recipe_food = RecipeFood.find_by(id: params[:id])
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @recipe_food = RecipeFood.includes(:recipe).find_by(id: params[:id])
+    puts @recipe
 
     if @recipe_food.update(recipe_food_params)
-      redirect_to recipe_path(@recipe), notice: 'Ingredient updated successfully'
+      flash[:notice] = 'Ingredient has been updated successfully'
+      redirect_to recipe_path(@recipe.id)
     else
-      flash[:alert] = 'Failed to update ingredient'
-      render :edit
+      flash[:alert] = 'Failed to update Ingredient!'
+      redirect_to recipe_path(@recipe)
     end
   end
 
   def destroy
-    @recipe_food = RecipeFood.find_by(id: params[:id])
+    @recipe_food = RecipeFood.includes(:recipe).find_by(id: params[:id])
 
     if @recipe_food.destroy
-      redirect_to recipe_path(id: params[:recipe_id]), notice: 'Ingredient removed successfully'
+      flash[:notice] = 'Ingedrient removed successfully.'
     else
-      redirect_to recipe_path(id: params[:recipe_id]), alert: 'Failed to delete the ingredient'
+      flash[:alert] = 'Failed to delete the ingredient!'
     end
+    redirect_to recipe_path(id: params[:recipe_id])
   end
 
   private
-
-  def find_recipe
-    @recipe = Recipe.find_by(id: params[:recipe_id])
-  end
 
   def recipe_food_params
     params.require(:recipe_food).permit(:quantity, :food_id)
