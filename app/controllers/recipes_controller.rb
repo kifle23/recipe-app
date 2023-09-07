beb1a169e7e1bdef26b28a32c982481f55c4e862
kifle23 <kiflekifle23@gmail.com>
2023-09-07 06:52:37 +0300

    Undo before action for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
     @recipes = Recipe.includes([:user]).where(user_id: current_user.id).order(created_at: :desc)
